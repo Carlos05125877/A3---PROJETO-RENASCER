92825b0499418443b0037fd40891d91b736cd1e8
Alex Fabiany Fernandes Filho <alexfernandesfilho38@gmail.com>
2025-10-08 00:06:42 -0300

    Adiciona declarações de módulos para imagens

---
+++ @types.d.ts
@@ -0,0 +1,41 @@
+// src/@types/images.d.ts
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.gif' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.webp' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.svg' {
+  import * as React from 'react';
+  const content: React.FC<React.SVGProps<SVGSVGElement>> & { url: string };
+  export default content;
+}
+
+declare module '*.bmp' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.tiff' {
+  const value: string;
+  export default value;
+}
